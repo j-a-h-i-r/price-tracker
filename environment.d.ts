b76976d21b5e959e51e38750fc095c45fc123430
Jahirul Islam <jahir.ewubd@gmail.com>
2025-03-31 23:51:38 +0200

    clean up opentelemetry setup

--- environment.d.ts
+++ environment.d.ts
@@ -2,18 +2,20 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
-            PORT?: number;
+            PORT?: string;
             DATABASE_URL: string;
             MAIL_HOST: string;
             MAIL_USER: string;
             MAIL_PASS: string;
-            MAIL_PORT: number;
+            MAIL_PORT: string;
             MAIL_FROM: string;
             MAIL_TO: string;
 
             SCRAPE_HOUR_INTERVAL: string;
 
             ADMIN_TOKEN: string;
+
+            SIGNOZ_ENDPOINT: string;
         }
     }
 }
