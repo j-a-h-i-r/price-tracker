876b3499a5381df73340c2fb8b56a9e99dbb1960
Jahirul Islam <jahir.ewubd@gmail.com>
2025-04-10 00:08:16 +0200

    replaced axios with undici

--- src/scrapers/techland.ts
+++ src/scrapers/techland.ts
@@ -15,8 +15,8 @@ export class Techland extends BaseScraper {
 
     private async fetchListingPageHtml(url: string, pageNumber: number): Promise<string> {
         const pageUrl = `${url}?page=${pageNumber}`;
-        const req = await this.axiosGet(pageUrl);
-        return req.data as string;
+        const req = await this.fetchWithThrottle(pageUrl);
+        return req.body.text();
     }
 
     private parsePageCount(html: string): number {
@@ -40,8 +40,9 @@ export class Techland extends BaseScraper {
     async parseProductPage(pageUrl: string): Promise<ScrapedProduct> {
         logger.debug(`Scraping ${pageUrl}`);
 
-        const req = await this.axiosGet(pageUrl);
-        const $ = cheerio.load(req.data);
+        const req = await this.fetchWithThrottle(pageUrl);
+        const html = await req.body.text();
+        const $ = cheerio.load(html);
 
         const productInfo: Record<string, string> = {};
         $('#product > table > tbody:nth-child(3) > tr').each((i, el) => {
