0a0fe7dca12eb6bd9c93aef6cc588ad9ce712c47
Jahirul Islam <jahir.ewubd@gmail.com>
2025-05-03 03:08:34 +0200

    feat: add admin authorization checks to metadata and price routes

--- src/api/potentialsimilar.ts
+++ src/api/potentialsimilar.ts
@@ -2,7 +2,10 @@ import { FastifyInstance } from 'fastify';
 import { ProductService } from '../services/product.service.js';
 
 export default async function statsRoutes(fastify: FastifyInstance) {
-    fastify.get('/', async () => {
+    fastify.get('/', async (req, res) => {
+        if (!req.isAdmin) {
+            return res.status(403).send({ message: 'You are not authorized to access this resource' });
+        }
         return new ProductService().getPossibleSimilarProducts();
     });
 }
