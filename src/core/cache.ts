91ffd7f15ffac4399424579befa49181da67c8b3
Jahirul Islam <jahir.ewubd@gmail.com>
2025-05-30 02:28:46 +0200

    Sex max cache size to 1000 items

--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,4 +1,5 @@
 import { CacheableMemory } from 'cacheable';
 export const cache = new CacheableMemory({
     ttl: 1000 * 60 * 60 * 6, // 6 hour. Should be invalidated when a new scrape runs
+    lruSize: 1000,
 });
